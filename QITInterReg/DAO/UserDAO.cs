ea1f3a38b84619f9f08f30d6c234205fc443f629
rajendrabaucha <rajendramanandhar7@gmail.com>
2020-03-25 10:10:37 +1000

    data access object added

---
+++ QITInterReg/DAO/UserDAO.cs
@@ -0,0 +1,24 @@
+﻿using QITInterReg.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QITInterReg.DAO
+{
+    public interface UserDAO
+    {
+
+        void insertUser(User user);
+
+        IEnumerable<User> getUsers();
+
+        User getUserById(int id);
+
+        void updateUser(User user);
+
+        User getUserByEmail(String email);
+
+        User getUserOnActivationSent(String id);
+    }
+}
